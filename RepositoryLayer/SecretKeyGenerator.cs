5039c33ba0d7b40acd8a62f9d9812597f28d7b0a
clavinsh <klavins.ar@gmail.com>
2024-05-26 20:51:10 +0300

    Created JWT Secret Key gen

---
+++ RepositoryLayer/SecretKeyGenerator.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RepositoryLayer
+{
+    internal class SecretKeyGenerator
+    {
+        public static string GenerateSecretKey(int length = 32) {
+            var key = new byte[length];
+            RandomNumberGenerator.Fill(key);
+            return Convert.ToBase64String(key);
+        }
+    }
+}
