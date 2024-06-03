a90df2f0f88aa8ffe118291dcd2173fcc2353a62
clavinsh <klavins.ar@gmail.com>
2024-06-03 22:21:26 +0300

    initial tests manager interface

---
+++ ServiceLayer/Interface/ITestsManager.cs
@@ -0,0 +1,14 @@
+﻿using MentalaisGidsAPI.Domain;
+
+namespace ServiceLayer.Interface
+{
+    public interface ITestsManager : IBaseManager<Tests>
+    {
+        Task<Tests> Get(int id);
+        Task<List<Tests>> GetAll();
+        Task<bool> Create(Tests test, int userId);
+        Task<bool> Submit(int userId); // vajag parametru kkādu atbilde DTO
+        Task<bool> Results(int userId);
+        Task<bool> Delete(int userId);
+    }
+}
---
+++ ServiceLayer/Manager/TestsManager.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MentalaisGidsAPI.Domain;
+using ServiceLayer.Interface;
+
+namespace ServiceLayer.Manager
+{
+    public class TestsManager : BaseManager<Tests>, ITestsManager { }
+}
